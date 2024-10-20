923e8d4f61b171d1180a71c241462d0818cc3b54
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-20 15:20:31 +0200

    simplified compress decompress utility

--- assets/js/alpine/usables/useCompressDecompress.js
+++ assets/js/alpine/usables/useCompressDecompress.js
@@ -1,37 +1,30 @@
 const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const padString = (input) => (6 - input.length % 6) == 6 ? input : input + "b".repeat(6 - input.length % 6);
 const stringToBinary = (input) => input.replace(/a/g, "0").replace(/b/g, "1");
-const binaryToDecimal = (binaryChunk) => parseInt(binaryChunk, 2);
-const decimalToBase64 = (decimal) => base64Chars[decimal];
-const base64ToDecimal = (base64Char) => base64Chars.indexOf(base64Char);
-const decimalToBinary = (decimal, bitSize) => decimal.toString(2).padStart(bitSize, "0");
-const groupBinaryString = (binaryStr, groupSize) => {
-  const numberOfChunks = Math.ceil(binaryStr.length / groupSize);
-  let chuncks = Array.from({ length: numberOfChunks });
-  return chuncks.map((_, index) => binaryStr.slice(index*groupSize, (index+1)*groupSize));
-}
+const binaryToDecimal = (input) => parseInt(input, 2);
+const decimalToBase64 = (input) => base64Chars[input];
+const base64ToDecimal = (input) => base64Chars.indexOf(input);
+const decimalToBinary = (input, bitSize) => input.toString(2).padStart(bitSize, "0");
+const chunkString = (input) => input.match(/.{6}/g) ?? [];
 
-export function compressString(input) {
-  // base64 uses 6 bits per character
-	// pad input with b so that it's length is a multiple of 6
-  input += "b".repeat(6 - input.length % 6);
-  // convert input into binary
-  const binaryString = stringToBinary(input);
-  // chunk binary
-  const binaryChunks = groupBinaryString(binaryString, 6);
-  // convert each binary chunk into a decimal and then base64 encode it
-  return binaryChunks
+export function compressString(inputString) {
+  // inputString will be a string made of a or b characters (i.e., the kts answers)
+  // need to pad string to be a multiple of 6 since base64 uses 6-bit characters
+  const paddedString = padString(inputString);
+  return chunkString(paddedString)
+    .map((input) => stringToBinary(input))
     .map(input => binaryToDecimal(input))
     .map(input => decimalToBase64(input))
-    .join("");
+    .join("")
 }
 
-export function decompressString(compressedStr, originalStringLength = 70) {
-  return compressedStr
+export function decompressString(compressedInput, originalInputLength = 70) {
+  return compressedInput
     .split("")
     .map(input => base64ToDecimal(input))
     .map(input => decimalToBinary(input, 6))
     .join("")
     .replace(/0/g, "a")
     .replace(/1/g, "b")
-    .slice(0, originalStringLength)
+    .slice(0, originalInputLength)
 }
