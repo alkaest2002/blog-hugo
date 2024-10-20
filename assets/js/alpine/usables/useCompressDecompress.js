7d3a530b3e3bb0c138b23d75f39f84a1a87b1183
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-20 20:00:24 +0200

    Update useCompressDecompress.js

--- assets/js/alpine/usables/useCompressDecompress.js
+++ assets/js/alpine/usables/useCompressDecompress.js
@@ -1,6 +1,5 @@
 const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-const padString = input => (6 - input.length % 6) == 6 ? input : input + "b".repeat(6 - input.length % 6);
-const chunkString = input => input.match(/.{6}/g) ?? [];
+const padString = input => (6 - input.length % 6) === 6 ? input : input + "b".repeat(6 - input.length % 6);
 const stringToBinary = input => input.replace(/a/g, "0").replace(/b/g, "1");
 const binaryToDecimal = input => parseInt(input, 2);
 const decimalToBase64 = input => base64Chars[input];
@@ -9,10 +8,10 @@ const decimalToBinary = input => input.toString(2).padStart(6, "0");
 const binaryToString = input => input.replace(/0/gi, "a").replace(/1/gi, "b");
 
 export function compressString(inputString) {
-  // inputString (already sanitized) will be a sequence of 70 "a" or "b" characters (i.e., the kts answers)
+  // inputString (must be already sanitized) will be a sequence of 70 "a" or "b" chars (i.e., the KTS answers)
   // need to pad inputString to make it a multiple of 6 since base64 uses 6-bit characters
-  const paddedString = padString(inputString);
-  return chunkString(paddedString)
+  return padString(inputString)
+    .match(/.{6}/g)
     .map(input => stringToBinary(input))
     .map(input => binaryToDecimal(input))
     .map(input => decimalToBase64(input))
